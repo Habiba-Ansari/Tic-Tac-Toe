2bd2a30e4d3e15a5e4819c7f7fc67479d8077024
Habiba Ansari <147974826+Habiba-Ansari@users.noreply.github.com>
2025-04-13 20:04:02 +0530

    Pushing all window files

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter_windows_3.27.4-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\flutter\\ttt\\tic_tac_toe" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter_windows_3.27.4-stable\\flutter"
+  "PROJECT_DIR=C:\\flutter\\ttt\\tic_tac_toe"
+  "FLUTTER_ROOT=C:\\flutter_windows_3.27.4-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\flutter\\ttt\\tic_tac_toe\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\flutter\\ttt\\tic_tac_toe"
+  "FLUTTER_TARGET=C:\\flutter\\ttt\\tic_tac_toe\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\flutter\\ttt\\tic_tac_toe\\.dart_tool\\package_config.json"
+)
